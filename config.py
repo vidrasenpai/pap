6024680631d0ddc24b7f5a88acfb13892055f7c9
vidrasenpai <88034866+vidrasenpai@users.noreply.github.com>
2022-08-18 07:22:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2128790575:AAEgNFiHd2VKk-j2Abr7-KVtgRTgNfq0z7Q")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5217057179:AAErwltKua9lIHsZF-2lRR_g_mY70farIUU")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "7375040"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "7375040"))
 API_HASH = os.environ.get("API_HASH", "4166e18db5a7880136d41ceb0aa20971")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001707214877"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001559696086"))
 
 # OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1880970848"))
@@ -31,12 +31,12 @@ OWNER = os.environ.get("OWNER", "Vidra")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://tffkqzix:QW3tqyjx4kM8tHDeN4_BRv6B559eYiJ8@satao.db.elephantsql.com/tffkqzix")
 
 # Username CH & Group
-CHANNEL = os.environ.get("CHANNEL", "indonbest")
-GROUP = os.environ.get("GROUP", "Hv015")
+CHANNEL = os.environ.get("CHANNEL", "paphunter_chanel")
+GROUP = os.environ.get("GROUP", "paphuntergc")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001322278013"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001787258151"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001766053397"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001585589164"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 
